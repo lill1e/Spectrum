a85e63ada38fef59500b9a25aca9e65bc05b4fbb
lillie <onlinechronically@protonmail.com>
2025-01-08 09:30:18 -0500

    add: more test items to support a wider variety

--- server/spectrum.lua
+++ server/spectrum.lua
@@ -16,6 +16,30 @@ Spectrum = {
                 name = "Spectrum:SetHealth",
                 data = { 100 }
             }
+        },
+        ["box"] = {
+            displayName = "Mystery Box",
+            illegal = false,
+            rare = false,
+            usable = true,
+            removeOnUse = false,
+            swapOnUse = {
+                item = "coin",
+                quantity = 5
+            },
+            handler = false,
+        },
+        ["coin"] = {
+            displayName = "Gold Coin",
+            illegal = false,
+            usable = false,
+            rare = false,
+            removeOnUse = false,
+            swapOnUse = false,
+            handler = function(source)
+                TriggerClientEvent("Spectrum:Notification", source,
+                    "You flip a coin and notice it landed on: ~y~" .. math.random(0, 1) and "Tails" or "Heads")
+            end,
         }
     },
     jobs = {
