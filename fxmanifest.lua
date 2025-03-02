615dde28084b67a0bfb20f69776494b8fa36ef4e
lillie <onlinechronically@protonmail.com>
2025-03-02 02:21:01 -0500

    add: Notification UI

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,7 +9,17 @@ description "wtf is this :smirk_cat:"
 shared_scripts {
     "shared/*.lua"
 }
+
 version "1.0.0"
+
+files {
+    "html/*.html",
+    "html/css/*.css",
+    "html/js/*.js",
+    "html/fonts/*.ttf"
+}
+
+ui_page "html/ui.html"
 
 client_scripts {
     "RageUI/RageUI.lua",
---
+++ html/js/app.js
@@ -0,0 +1,42 @@
+(() => {
+    function displayInventoryNotification(type, itemType, name, quantity) {
+        let notif = ""
+
+        if (type == 1) {
+            notif += "+"
+        } else {
+            notif += "-"
+        }
+
+        if (itemType == 3) {
+            notif += `${name} Ammo (${quantity} Rounds)`
+        }
+        else if (quantity != -1) {
+            notif += quantity + " " + name
+        }
+        else {
+            notif += " " + name
+        }
+
+        let elem = $("<div>" + notif + "</div>")
+        $("#inventory_notifications").append(elem)
+
+        $(elem)
+            .delay(3000)
+            .fadeOut(1000, () => {
+                elem.remove()
+            })
+    }
+
+    window.onData = (data) => {
+        if (data.action === "notification") {
+            displayInventoryNotification(data.type, data.itemType, data.item, data.quantity);
+        }
+    };
+
+    window.onload = function(e) {
+        window.addEventListener("message", (event) => {
+            onData(event.data);
+        });
+    };
+})();
