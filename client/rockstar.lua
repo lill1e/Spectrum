e2929d0ff15f0b9146fefe182287574dfffbaf25
lillie <onlinechronically@protonmail.com>
2025-03-24 14:02:53 -0400

    add: disables of native GTA features

---
+++ client/rockstar.lua
@@ -0,0 +1,19 @@
+for i = 1, 15 do
+    EnableDispatchService(i, false)
+end
+
+DisplayCash(false)
+
+SetWeaponsNoAutoreload(true)
+SetWeaponsNoAutoswap(true)
+
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+        ClearPlayerWantedLevel(PlayerId())
+        SetMaxWantedLevel(0)
+
+        SetPlayerHealthRechargeMultiplier(PlayerId(), 0.0)
+    end
+end)
+
