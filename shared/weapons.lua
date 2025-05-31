71a2bcd483c770de8e0e7452f86c6bbb58c3ac0c
lillie <onlinechronically@protonmail.com>
2025-05-31 05:11:37 -0400

    move: weapon names to a different file

---
+++ shared/weapons.lua
@@ -0,0 +1,357 @@
+Config.Weapons = {
+    WEAPON_DAGGER = {
+        displayName = "Antique Cavalry Dagger",
+    },
+    WEAPON_BAT = {
+        displayName = "Baseball Bat",
+    },
+    WEAPON_BOTTLE = {
+        displayName = "Broken Bottle",
+    },
+    WEAPON_CROWBAR = {
+        displayName = "Crowbar",
+    },
+    WEAPON_UNARMED = {
+        displayName = "Fist",
+    },
+    WEAPON_FLASHLIGHT = {
+        displayName = "Flashlight",
+    },
+    WEAPON_GOLFCLUB = {
+        displayName = "Golf Club",
+    },
+    WEAPON_HAMMER = {
+        displayName = "Hammer",
+    },
+    WEAPON_HATCHET = {
+        displayName = "Hatchet",
+    },
+    WEAPON_KNUCKLE = {
+        displayName = "Brass Knuckles",
+    },
+    WEAPON_KNIFE = {
+        displayName = "Knife",
+    },
+    WEAPON_MACHETE = {
+        displayName = "Machete",
+    },
+    WEAPON_SWITCHBLADE = {
+        displayName = "Switchblade",
+    },
+    WEAPON_NIGHTSTICK = {
+        displayName = "Nightstick",
+    },
+    WEAPON_WRENCH = {
+        displayName = "Pipe Wrench",
+    },
+    WEAPON_BATTLEAXE = {
+        displayName = "Battle Axe",
+    },
+    WEAPON_POOLCUE = {
+        displayName = "Pool Cue",
+    },
+    WEAPON_STONE_HATCHET = {
+        displayName = "Stone Hatchet",
+    },
+    WEAPON_PISTOL = {
+        displayName = "Pistol",
+    },
+    WEAPON_PISTOL_MK2 = {
+        displayName = "Pistol Mk II",
+    },
+    WEAPON_COMBATPISTOL = {
+        displayName = "Combat Pistol",
+    },
+    WEAPON_APPISTOL = {
+        displayName = "AP Pistol",
+    },
+    WEAPON_STUNGUN = {
+        displayName = "Stun Gun",
+    },
+    WEAPON_PISTOL50 = {
+        displayName = "Pistol .50",
+    },
+    WEAPON_SNSPISTOL = {
+        displayName = "SNS Pistol",
+    },
+    WEAPON_SNSPISTOL_MK2 = {
+        displayName = "SNS Pistol Mk II",
+    },
+    WEAPON_HEAVYPISTOL = {
+        displayName = "Heavy Pistol",
+    },
+    WEAPON_VINTAGEPISTOL = {
+        displayName = "Vintage Pistol",
+    },
+    WEAPON_FLAREGUN = {
+        displayName = "Flare Gun",
+    },
+    WEAPON_MARKSMANPISTOL = {
+        displayName = "Marksman Pistol",
+    },
+    WEAPON_REVOLVER = {
+        displayName = "Heavy Revolver",
+    },
+    WEAPON_REVOLVER_MK2 = {
+        displayName = "Heavy Revolver Mk II",
+    },
+    WEAPON_DOUBLEACTION = {
+        displayName = "Double Action Revolver",
+    },
+    WEAPON_RAYPISTOL = {
+        displayName = "Up-n-Atomizer",
+    },
+    WEAPON_CERAMICPISTOL = {
+        displayName = "Ceramic Pistol",
+    },
+    WEAPON_NAVYREVOLVER = {
+        displayName = "Navy Revolver",
+    },
+    WEAPON_GADGETPISTOL = {
+        displayName = "Perico Pistol",
+    },
+    WEAPON_STUNGUN_MP = {
+        displayName = "Stun Gun",
+    },
+    WEAPON_MICROSMG = {
+        displayName = "Micro SMG",
+    },
+    WEAPON_SMG = {
+        displayName = "SMG",
+    },
+    WEAPON_SMG_MK2 = {
+        displayName = "SMG Mk II",
+    },
+    WEAPON_ASSAULTSMG = {
+        displayName = "Assault SMG",
+    },
+    WEAPON_COMBATPDW = {
+        displayName = "Combat PDW",
+    },
+    WEAPON_MACHINEPISTOL = {
+        displayName = "Machine Pistol",
+    },
+    WEAPON_MINISMG = {
+        displayName = "Mini SMG",
+    },
+    WEAPON_RAYCARBINE = {
+        displayName = "Unholy Hellbringer",
+    },
+    WEAPON_PUMPSHOTGUN = {
+        displayName = "Pump Shotgun",
+    },
+    WEAPON_PUMPSHOTGUN_MK2 = {
+        displayName = "Pump Shotgun Mk II",
+    },
+    WEAPON_SAWNOFFSHOTGUN = {
+        displayName = "Sawed-Off Shotgun",
+    },
+    WEAPON_ASSAULTSHOTGUN = {
+        displayName = "Assault Shotgun",
+    },
+    WEAPON_BULLPUPSHOTGUN = {
+        displayName = "Bullpup Shotgun",
+    },
+    WEAPON_MUSKET = {
+        displayName = "Musket",
+    },
+    WEAPON_HEAVYSHOTGUN = {
+        displayName = "Heavy Shotgun",
+    },
+    WEAPON_DBSHOTGUN = {
+        displayName = "Double Barrel Shotgun",
+    },
+    WEAPON_AUTOSHOTGUN = {
+        displayName = "Sweeper Shotgun",
+    },
+    WEAPON_COMBATSHOTGUN = {
+        displayName = "Combat Shotgun",
+    },
+    WEAPON_ASSAULTRIFLE = {
+        displayName = "Assault Rifle",
+    },
+    WEAPON_ASSAULTRIFLE_MK2 = {
+        displayName = "Assault Rifle Mk II",
+    },
+    WEAPON_CARBINERIFLE = {
+        displayName = "Carbine Rifle",
+    },
+    WEAPON_CARBINERIFLE_MK2 = {
+        displayName = "Carbine Rifle Mk II",
+    },
+    WEAPON_ADVANCEDRIFLE = {
+        displayName = "Advanced Rifle",
+    },
+    WEAPON_SPECIALCARBINE = {
+        displayName = "Special Carbine",
+    },
+    WEAPON_SPECIALCARBINE_MK2 = {
+        displayName = "Special Carbine Mk II",
+    },
+    WEAPON_BULLPUPRIFLE = {
+        displayName = "Bullpup Rifle",
+    },
+    WEAPON_BULLPUPRIFLE_MK2 = {
+        displayName = "Bullpup Rifle Mk II",
+    },
+    WEAPON_COMPACTRIFLE = {
+        displayName = "Compact Rifle",
+    },
+    WEAPON_MILITARYRIFLE = {
+        displayName = "Military Rifle",
+    },
+    WEAPON_HEAVYRIFLE = {
+        displayName = "Heavy Rifle",
+    },
+    WEAPON_MG = {
+        displayName = "MG",
+    },
+    WEAPON_COMBATMG = {
+        displayName = "Combat MG",
+    },
+    WEAPON_COMBATMG_MK2 = {
+        displayName = "Combat MG Mk II",
+    },
+    WEAPON_GUSENBERG = {
+        displayName = "Gusenberg Sweeper",
+    },
+    WEAPON_SNIPERRIFLE = {
+        displayName = "Sniper Rifle",
+    },
+    WEAPON_HEAVYSNIPER = {
+        displayName = "Heavy Sniper",
+    },
+    WEAPON_HEAVYSNIPER_MK2 = {
+        displayName = "Heavy Sniper Mk II",
+    },
+    WEAPON_MARKSMANRIFLE = {
+        displayName = "Marksman Rifle",
+    },
+    WEAPON_MARKSMANRIFLE_MK2 = {
+        displayName = "Marksman Rifle Mk II",
+    },
+    WEAPON_RPG = {
+        displayName = "RPG",
+    },
+    WEAPON_GRENADELAUNCHER = {
+        displayName = "Grenade Launcher",
+    },
+    WEAPON_GRENADELAUNCHER_SMOKE = {
+        displayName = "Grenade Launcher Smoke",
+    },
+    WEAPON_MINIGUN = {
+        displayName = "Minigun",
+    },
+    WEAPON_FIREWORK = {
+        displayName = "Firework Launcher",
+    },
+    WEAPON_RAILGUN = {
+        displayName = "Railgun",
+    },
+    WEAPON_HOMINGLAUNCHER = {
+        displayName = "Homing Launcher",
+    },
+    WEAPON_COMPACTLAUNCHER = {
+        displayName = "Compact Grenade Launcher",
+    },
+    WEAPON_RAYMINIGUN = {
+        displayName = "Widowmaker",
+    },
+    WEAPON_EMPLAUNCHER = {
+        displayName = "Compact EMP Launcher",
+    },
+    WEAPON_GRENADE = {
+        displayName = "Grenade",
+    },
+    WEAPON_BZGAS = {
+        displayName = "BZ Gas",
+    },
+    WEAPON_MOLOTOV = {
+        displayName = "Molotov Cocktail",
+    },
+    WEAPON_STICKYBOMB = {
+        displayName = "Sticky Bomb",
+    },
+    WEAPON_PROXMINE = {
+        displayName = "Proximity Mines",
+    },
+    WEAPON_SNOWBALL = {
+        displayName = "Snowballs",
+    },
+    WEAPON_PIPEBOMB = {
+        displayName = "Pipe Bombs",
+    },
+    WEAPON_BALL = {
+        displayName = "Baseball",
+    },
+    WEAPON_SMOKEGRENADE = {
+        displayName = "Tear Gas",
+    },
+    WEAPON_FLARE = {
+        displayName = "Flare",
+    },
+    WEAPON_PETROLCAN = {
+        displayName = "Jerry Can",
+    },
+    GADGET_PARACHUTE = {
+        displayName = "Parachute",
+    },
+    WEAPON_FIREEXTINGUISHER = {
+        displayName = "Fire Extinguisher",
+    },
+    WEAPON_HAZARDCAN = {
+        displayName = "Hazardous Jerry Can",
+    },
+    WEAPON_FERTILIZERCAN = {
+        displayName = "Fertilizer Can",
+    },
+
+    -- mpsum2
+    WEAPON_TACTICALRIFLE = {
+        displayName = "Service Carbine",
+    },
+    WEAPON_PRECISIONRIFLE = {
+        displayName = "Precision Rifle",
+    },
+    WEAPON_METALDETECTOR = {
+        displayName = "Metal Detector",
+    },
+
+    -- mpchristmas3
+    WEAPON_PISTOLXM3 = {
+        displayName = "WM 29 Pistol",
+    },
+    WEAPON_CANDYCANE = {
+        displayName = "Candy Cane",
+    },
+    WEAPON_RAILGUNXM3 = {
+        displayName = "Railgun",
+    },
+    WEAPON_ACIDPACKAGE = {
+        displayName = "Acid Package",
+    },
+
+    -- mp2023_01
+    WEAPON_TECPISTOL = {
+        displayName = "Tactical SMG",
+    },
+
+    AMMO_PISTOL = {
+        displayName = "Handgun"
+    },
+    AMMO_SHOTGUN = {
+        displayName = "Shotgun"
+    },
+    AMMO_SMG = {
+        displayName = "SMG"
+    },
+    AMMO_RIFLE = {
+        displayName = "Rifle"
+    },
+    AMMO_SNIPER = {
+        displayName = "Sniper Rifle"
+    },
+    AMMO_MG = {
+        displayName = "Machine Gun"
+    }
+}
