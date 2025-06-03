7b4b42478d58a22faa441ebca60f0621e0881649
lillie <onlinechronically@protonmail.com>
2025-06-03 01:08:26 -0400

    add: GetClosestVehicle (Server)

--- server/functions.lua
+++ server/functions.lua
@@ -13,3 +13,26 @@ end
 function Notification(source, text)
     TriggerClientEvent("Spectrum:Notification", source, text)
 end
+
+function GetClosestVehicle(source)
+    if GetVehiclePedIsIn(GetPlayerPed(source), false) ~= 0 then
+        return GetVehiclePedIsIn(GetPlayerPed(source), false)
+    end
+    local entPool = GetAllVehicles()
+    local selfCoords = GetEntityCoords(GetPlayerPed(source))
+    local minDist = nil
+    local minHandle = nil
+    for _, handle in ipairs(entPool) do
+        local entCoords = GetEntityCoords(handle)
+        local d = #(selfCoords - entCoords)
+        if minDist == nil or d < minDist then
+            minDist = d
+            minHandle = handle
+        end
+    end
+    if minHandle and minDist <= 3 then
+        return minHandle
+    else
+        return nil
+    end
+end
