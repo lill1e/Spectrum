44b4b4d3ea156b6f9171d0bf0b296bcac1f74632
lillie <onlinechronically@protonmail.com>
2025-06-07 03:19:59 -0400

    add: Weapon Ammo types to config

---
+++ shared/weaponAmmo.lua
@@ -0,0 +1,8 @@
+Config.Ammo = {
+    [GetHashKey("AMMO_MG")] = "AMMO_MG",
+    [GetHashKey("AMMO_SMG")] = "AMMO_SMG",
+    [GetHashKey("AMMO_RIFLE")] = "AMMO_RIFLE",
+    [GetHashKey("AMMO_PISTOL")] = "AMMO_PISTOL",
+    [GetHashKey("AMMO_SNIPER")] = "AMMO_SNIPER",
+    [GetHashKey("AMMO_SHOTGUN")] = "AMMO_SHOTGUN"
+}
