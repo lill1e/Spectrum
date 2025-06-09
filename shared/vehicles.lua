7f63ffb657cc9a991d07d63c0aea49f701b630ba
lillie <onlinechronically@protonmail.com>
2025-06-08 21:05:09 -0400

    remove: Horns from vehicle mod menu

--- shared/vehicles.lua
+++ shared/vehicles.lua
@@ -924,7 +924,6 @@ Config.Vehicles = {
             [11] = "Engine",
             [12] = "Brakes",
             [13] = "Transmission",
-            [14] = "Horns",
             [15] = "Suspension",
             [16] = "Armor",
             [18] = "Turbo",
@@ -957,7 +956,7 @@ Config.Vehicles = {
         ModCategories = {
             Performance = { 11, 12, 13, 15, 18 },
             Appearance = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 48 },
-            ["Appearance (Extra)"] = { 14, 22 },
+            ["Appearance (Extra)"] = { 22 },
             Other = { 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46 }
         }
     }
